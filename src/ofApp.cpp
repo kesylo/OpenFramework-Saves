2b65d549da13478eeb09aaf1452e5bfda11c15bd
Kesylo <keming.loic@yahoo.com>
2019-10-07 22:20:34 +0200

    Create image pixel by pixel

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,28 +1,59 @@
 #include "ofApp.h"
+#include <cstdlib>
+
 // variables
 ofImage image;
+int w = 512;
+int h = 512;
+
+
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-	// load image
-	image.loadImage("githubLogo.png");
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-	
+
+	// array of pixels to be filled in our image
+	// An image total pixel count = width * height
+	// we multiply * 4 because we want to use the for channels OF_IMAGE_COLOR_ALPHA
+	unsigned char *data = new unsigned char[w * h * 4];
+
+	// loop through and fill array
+	for (int y = 0; y < h; y++)			// vertical loop
+	{
+		for (int x = 0; x < w; x++)		// horizontal loop
+		{
+			// set any value for each channel 
+			int red = rand() % 256;		// red is in the range 0 to 255;
+			int green = rand() % 256;
+			int blue = rand() % 256;
+			int alpha = 255;
+
+			// add every color to their position in array [r, g, b, a]
+			int index = 4 * (x + w * y);		// Static
+			data[index] = red;
+			data[index + 1] = green;
+			data[index + 2] = blue;
+			data[index + 3] = alpha;
+		}
+	}
+
+	// load array into image
+	image.setFromPixels(data, w, h, OF_IMAGE_COLOR_ALPHA);
+
+	// delete array to free memory
+	delete[] data;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-	// set canvas backgroung RBG
-	ofBackground(255, 255, 255);
+	ofSetBackgroundColor(255, 255, 255);
 
-	// draw image with one color chanel
-	ofSetColor(255, 0, 0); // red' = red / 255 = 1 so max value
+	ofSetColor(255, 255, 255);		// display image with color
 
-	// draw image in canvas at position 0, 0
-	image.draw(0, 0);
+	image.draw(0, 0);		// draw image in canvas at position 0, 0
 }
 
 //--------------------------------------------------------------
