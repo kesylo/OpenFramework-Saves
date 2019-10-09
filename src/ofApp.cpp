721588a3b4bd0b24b2e8d9797aafda3d53912d5b
Kesylo <keming.loic@yahoo.com>
2019-10-10 00:13:48 +0200

    RGB to GrayScale with ofColor

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -5,8 +5,9 @@ ofImage image;
 ofImage imageCopy;
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-	image.load("githubLogo.png");
+void ofApp::setup() {
+
+	image.load("Rainbow.jpg");
 	// set imageCopy size and type. this does not copy the image content
 	// it just reserve space in memory
 	imageCopy.allocate(image.getWidth(), image.getHeight(), OF_IMAGE_COLOR_ALPHA);
@@ -17,8 +18,16 @@ void ofApp::setup(){
 		{
 			// get color RGBA of each pixel a position xy
 			ofColor imageColor = image.getColor(x, y);
+
+			// turn rgb pixel to grayScale
+			// eg: pixel: [122, 23, 67] becomes [70.6, 70.6, 70.6]
+			int grayScale = (imageColor.r + imageColor.g + imageColor.b) / 3;
+
+			// create grayscaled color
+			ofColor newColor(grayScale, grayScale, grayScale);
+
 			// set it on the new image
-			imageCopy.setColor(x, y, imageColor);
+			imageCopy.setColor(x, y, newColor);
 		}
 	}
 	// update edited image
@@ -26,12 +35,12 @@ void ofApp::setup(){
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-	
+void ofApp::update() {
+
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 	ofSetBackgroundColor(255, 255, 255);		// set windows BG white
 
 	ofSetColor(255, 255, 255);		// display image with color
@@ -42,56 +51,56 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
+void ofApp::mouseEntered(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
+void ofApp::mouseExited(int x, int y) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
