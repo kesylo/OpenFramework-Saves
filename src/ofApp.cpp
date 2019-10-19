ddd286c8c5ec089f9d086aa45e8792b547c4c5ac
Kesylo <keming.loic@yahoo.com>
2019-10-19 02:21:56 +0200

    complete refactor. using resharper, vectors, adaptative binarisation - done

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,38 +1,32 @@
 #include "ofApp.h"
 #include "ofxGrafica.h"
+#include <iterator>
+#include <numeric>
+#include <algorithm>
 
-ofImage mainImage;
-ofImage grayImage;
-ofImage lutImage;
-int grayAddedValue = 0;
-bool drawMainImage = false; 
-bool drawGrayImage = false;
-bool drawLUT = false;
+#pragma region variables
+ofImage main_image;
+ofImage main_image_clone;
+ofImage gray_image;
+ofImage lut_image;
+ofImage a_binarisation_image;
+int gray_added_value = 0;
+bool draw_main_image = false;
+bool draw_main_image_clone = false;
+bool draw_gray_image = false;
+bool draw_lut = false;
+bool draw_a_bin_image = false;
+#pragma endregion
+
 
 //--------------------------------------------------------------
 void ofApp::setup() {
 	// create gui and add elements to it
 	gui.setup();
-	gui.add(intSlider.setup("Grayscale Slider", 0, 0, 255));
+	gui.add(int_slider.setup("Grayscale Slider", 0, 0, 255));
 	
 	// load original image
-	mainImage.load("SinCity.jpg");
-
-	grayAddedValue = 3;
-
-	if (grayAddedValue > 1 && grayAddedValue < 5)
-	{
-		cout << "yess";
-	}
-
-	//------------------------------------------------------------------------
-
-	/*int arr[10] = { 3,4,1,2,4,5,6,5,4,5 };
-
-	cout << countValueOccurences(5, arr, 10);*/
-	
-
-	
+	main_image.load("coins2.jpg");
 
 	
 }
@@ -48,27 +42,33 @@ void ofApp::draw() {
 	ofSetBackgroundColor(255);
 	ofSetColor(255);
 
-	if (drawMainImage)
+	if (draw_main_image)
 	{
-		mainImage.draw(0, 0);
+		main_image.draw(0, 0);
+	}
+	if (draw_main_image_clone)
+	{
+		main_image_clone.draw(main_image.getWidth() + 10, 0);
+	}
+	if (draw_gray_image)
+	{
+		gray_image.draw(main_image.getWidth() + 10, 0);
+	}
+	if (draw_lut)
+	{
+		lut_image.draw(main_image.getWidth() + 10, 0);
+	}
+	if (draw_a_bin_image)
+	{
+		a_binarisation_image.draw(main_image.getWidth() + 10, 0);
 	}
 
-	if (drawGrayImage)
-	{
-		grayImage.draw(mainImage.getWidth() + 10, 0);
-	}
-
-	if (drawLUT)
-	{
-		lutImage.draw(mainImage.getWidth() + 10, 0);
-	}
 	
 	
-
 	
 	// display plot
-	/*plot.setDim(500, 300);
-	plot.setPos(mainImage.getWidth(), 0);
+	plot.setDim(300, 300);
+	plot.setPos(0, 300);
 	plot.beginDraw();
 	plot.drawBackground();
 	plot.drawBox();
@@ -77,193 +77,268 @@ void ofApp::draw() {
 	plot.drawTitle();
 	plot.drawHistograms();
 	plot.endDraw();
-	*/
 	
 	// draw gui 
 	gui.draw();
 }
 
-//--------------------------------------------------------------
 void ofApp::keyPressed(int key) {
 	if (key == '7')
 	{
-		drawMainImage = true;
+		draw_main_image = true;
+		draw();
+	}
+	if (key == 'r')
+	{
+		draw_main_image_clone = true;
 		draw();
 	}
 	if (key == '1')
 	{
 		// Display gray version of Image
-		mainImage.load("hue.jpg");
-		grayImage = getGrayScaledImageOf(mainImage);
+		gray_image = get_gray_scaled_image_of(main_image);
 		draw();
 	}
 	if (key == OF_KEY_UP)
 	{
-		grayAddedValue += 50;
-		grayImage = getGrayScaledImageOf(mainImage);
+		gray_added_value += 50;
+		gray_image = get_gray_scaled_image_of(main_image);
 		draw();
 	}
-	if (key == 'd')
+	if (key == 'h')
 	{
 		// show histogram of an image
-		mainImage.load("SinCity.jpg");		// load image
-		grayImage = getGrayScaledImageOf(mainImage);		// convert to gray
-		showHistogramOf(grayImage);		// display histogram
+		gray_image = get_gray_scaled_image_of(main_image);
+		show_histogram_of(gray_image);
 	}
 	if (key == 'b')
 	{
 		// draw LUT Black and White
-		lutImage = LUTBlackAndWhite(mainImage);
-		drawLUT = true;
+		lut_image = lut_black_and_white(main_image, 127);
+		draw_lut = true;
 		draw();
 	}
 	if (key == 'v')
 	{
 		// draw LUT Black and White
-		lutImage = LUTVShape(mainImage);
-		drawLUT = true;
+		lut_image = lut_v_shape(main_image);
+		draw_lut = true;
+		draw();
+	}
+	if (key == 'a')
+	{
+		// Adaptive Binarisation
+		a_binarisation_image = adaptive_binarisation(main_image);
+		draw_a_bin_image = true;
 		draw();
 	}
 }
 
+/*-----------------------------------------------------------------------------------------------------*/
 
+std::vector<int> ofApp::get_histogram_array(const ofImage& gray_image)
+{
+	const int w = gray_image.getWidth();
+	const int h = gray_image.getHeight();
 
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key) {
+	// Prepare the points for the plot
+	const auto x_axis_points = 256;
+	vector<ofxGPoint> points;
+	int histogram[x_axis_points] = { 0 };
 
+	// get all pixels in array
+	const auto& list_pixels = gray_image.getPixels();
+
+	for (auto y = 0; y < h; y++)
+	{
+		for (auto x = 0; x < w; x++)
+		{
+			histogram[list_pixels.getColor(x, y).r] += 1;
+		}
+	}
+
+	std::vector<int> histogram_v(histogram, histogram + 256);
+
+	return histogram_v;
 }
 
-int ofApp::countValueOccurences(int num, int* arr, int length) {
-	if (!length)
-		return 0;
-	int c = countValueOccurences(num, arr + 1, length - 1);
-	return arr[0] == num ? c + 1 : c;
+ofImage ofApp::resize_image(ofImage image_in, const float scale_down_value) {
+	image_in.resize(image_in.getWidth() / scale_down_value, image_in.getHeight() / scale_down_value);
+	return image_in;
 }
 
-ofImage ofApp::getGrayScaledImageOf(ofImage image) {
-	int w = image.getWidth();
-	int h = image.getHeight();
-	ofImage imgCopy;
-	ofColor grayColor;
-	int grayLevel;
+ofImage ofApp::get_gray_scaled_image_of(ofImage image) {
+	auto w = image.getWidth();
+	auto h = image.getHeight();
+	ofImage img_copy;
+	ofColor gray_color;
+	int gray_level;
 	int i;
 
 	// get all pixels in array
-	ofPixels_<unsigned char> listPixels = image.getPixels();
+	ofPixels_<unsigned char> list_pixels = image.getPixels();
 	// create array for all pixels color
 	ofColor* pixelColors = new ofColor[w * h];
 
-	for (int y = 0; y < h; y++)
+	for (auto y = 0; y < h; y++)
 	{
-		for (int x = 0; x < w; x++)
+		for (auto x = 0; x < w; x++)
 		{
 			// for loop in colors
 			i = y * w + x;
 
 			// get color of each pixel a position xy
-			pixelColors[i] = listPixels.getColor(x, y);
+			pixelColors[i] = list_pixels.getColor(x, y);
 
 			// easy get grey level
-			grayLevel = pixelColors[i].getLightness();
+			gray_level = pixelColors[i].getLightness();
 
 			// create grey color
-			grayColor = (grayLevel , grayLevel, grayLevel);
+			gray_color = (gray_level , gray_level, gray_level);
 
 			// set it on pixel list
-			listPixels.setColor(x, y, grayColor);
+			list_pixels.setColor(x, y, gray_color);
 		}
 	}
 	// create image copy from edited pixel array 
-	imgCopy.setFromPixels(listPixels);
+	img_copy.setFromPixels(list_pixels);
 
-	return imgCopy;
+	return img_copy;
 }
 
-ofImage ofApp::LUTBlackAndWhite(ofImage imageIn)
+ofImage ofApp::lut_black_and_white(const ofImage& image_in, const int threshold)
 {
-	int grayLevel;
-	ofColor black = ofColor::black;
-	ofColor white = ofColor::white;
-	ofImage imageOut;
-	imageOut.allocate(imageIn.getWidth(), imageIn.getHeight(), OF_IMAGE_COLOR);
+	int gray_level;
+	auto black = ofColor::black;
+	auto white = ofColor::white;
+	ofImage image_out;
+	image_out.allocate(image_in.getWidth(), image_in.getHeight(), OF_IMAGE_COLOR);
 
-	for (int y = 0; y < imageIn.getHeight(); y++)
+	for (auto y = 0; y < image_in.getHeight(); y++)
 	{
-		for (int x = 0; x < imageIn.getWidth(); x++)
+		for (auto x = 0; x < image_in.getWidth(); x++)
 		{
-			grayLevel = imageIn.getColor(x, y).getLightness();
+			gray_level = image_in.getColor(x, y).getLightness();
 
-			if (grayLevel <= 127)
+			if (gray_level <= threshold)
 			{
-				imageOut.setColor(x, y, black);
+				image_out.setColor(x, y, black);
 			}
 			else
 			{
-				imageOut.setColor(x, y, white);
+				image_out.setColor(x, y, white);
 			}
 		}
 	}
-	imageOut.update();
+	image_out.update();
 
-	return imageOut;
+	return image_out;
 }
 
-ofImage ofApp::LUTVShape(ofImage imageIn)
+ofImage ofApp::lut_v_shape(const ofImage& image_in)
 {
-	int grayLevel;
-	int start = 0;
-	int middleLeft = 65;
-	int middleRight = 126;
-	int end = 255;
-	ofImage imageOut;
-	ofColor black = ofColor::black;
-	ofColor white = ofColor::white;
-	imageOut.allocate(imageIn.getWidth(), imageIn.getHeight(), OF_IMAGE_COLOR);
+	int gray_level;
+	auto start = 0;
+	auto middle_left = 65;
+	auto middle_right = 126;
+	auto end = 255;
+	ofImage image_out;
+	auto black = ofColor::black;
+	auto white = ofColor::white;
 
-	for (int y = 0; y < imageIn.getHeight(); y++)
+	image_out.allocate(image_in.getWidth(), image_in.getHeight(), OF_IMAGE_COLOR);
+
+	for (auto y = 0; y < image_in.getHeight(); y++)
 	{
-		for (int x = 0; x < imageIn.getWidth(); x++)
+		for (auto x = 0; x < image_in.getWidth(); x++)
 		{
-			grayLevel = imageIn.getColor(x, y).getLightness();
+			gray_level = image_in.getColor(x, y).getLightness();
 
-			if (grayLevel >= start && grayLevel <= middleLeft || grayLevel >= middleRight && grayLevel <= end)
+			if (gray_level >= start && gray_level <= middle_left || gray_level >= middle_right && gray_level <= end)
 			{
-				imageOut.setColor(x, y, white);
+				image_out.setColor(x, y, white);
 			}
-			else if (grayLevel >= middleLeft && grayLevel <= middleRight)
+			else if (gray_level >= middle_left && gray_level <= middle_right)
 			{
-				imageOut.setColor(x, y, black);
+				image_out.setColor(x, y, black);
 			}
 		}
 	}
-	imageOut.update();
+	image_out.update();
 
-	return imageOut;
+	return image_out;
 }
 
-void ofApp::drawPlotOnScreen(vector<ofxGPoint> points, string plotName, string xAxisName, string yAxisName, int plotHeight) {
+ofImage ofApp::adaptive_binarisation(const ofImage& image_in)
+{
+	ofImage out_image;
+
+	// get vector array from histogram. size is 256 always
+	auto histogram_values = get_histogram_array(image_in);
+
+	// split in 2
+	auto const half_size = histogram_values.size() / 2;
+	std::vector<int> part_1(histogram_values.begin(), histogram_values.begin() + half_size);
+	std::vector<int> part_2(histogram_values.begin() + half_size, histogram_values.end());
+
+	// get the max value in each array
+	const auto max_1 = * max_element(part_1.begin(), part_1.end());
+	const auto max_2 = * max_element(part_2.begin(), part_2.end());
+
+	// find element in in vector
+	const auto it_1 = std::find(histogram_values.begin(), histogram_values.end(), max_1);
+	const auto it_2 = std::find(histogram_values.begin(), histogram_values.end(), max_2);
+
+	// Get index of element from iterator
+	const auto index_1 = std::distance(histogram_values.begin(), it_1);
+	const auto index_2 = std::distance(histogram_values.begin(), it_2);
+
+	// make average to find interval right middle
+	int avg = (index_2 + index_1) / 2;
+
+	// show image values
+	/*for (int v : histogram_values)
+	{
+		cout << v << " - ";
+	}*/
+
+	cout << " \n";
+	cout << max_1 << " max 1 val \n";
+	cout << max_2 << " max 2 \n";
+	cout << index_1 << " pos max 1 \n";
+	cout << index_2 << " pos max 2 \n";
+	cout << avg << " avg of indexes between both max \n";
+
+	// set grayscale threshold to 
+	out_image = lut_black_and_white(image_in, avg);
+
+	return out_image;
+}
+
+void ofApp::draw_plot_on_screen(const vector<ofxGPoint>& points, string plot_name,  string x_axis_name,  string y_axis_name, const int plot_height)
+{
 	// Add the points
 	plot.setPoints(points);
 
 	// Set the plot title and the axis labels
-	plot.setTitleText(plotName);
-	plot.getXAxis().setAxisLabelText(xAxisName);
-	plot.getYAxis().setAxisLabelText(yAxisName);
+	plot.setTitleText(plot_name);
+	plot.getXAxis().setAxisLabelText(x_axis_name);
+	plot.getYAxis().setAxisLabelText(y_axis_name);
 	plot.getTitle().setTextAlignment(GRAFICA_LEFT_ALIGN);
 	plot.startHistograms(GRAFICA_VERTICAL_HISTOGRAM);
-	plot.setYLim(0, plotHeight);
+	plot.setYLim(0, plot_height);
 	plot.setXLim(1, 300);
 	plot.getHistogram().setDrawLabels(true);
 	plot.getHistogram().setRotateLabels(true);
 	
 	plot.getHistogram().setBgColors({ ofColor(0, 0, 255, 50), ofColor(0, 0, 255, 100), ofColor(0, 0, 255, 150), ofColor(0, 0, 255, 200) }); // Je définis la couleur des rectangles.
 
-	// Set the plot position on the screen
+	// returns void
 }
 
-void ofApp::showHistogramOf(ofImage grayImage) {
-	int w = grayImage.getWidth();
-	int h = grayImage.getHeight();
+void ofApp::show_histogram_of(ofImage gray_image) {
+	int w = gray_image.getWidth();
+	int h = gray_image.getHeight();
 
 	// Prepare the points for the plot
 	const int xAxisPoints = 256;
@@ -271,7 +346,7 @@ void ofApp::showHistogramOf(ofImage grayImage) {
 	int histogram[xAxisPoints] = {0};
 
 	// get all pixels in array
-	ofPixels_<unsigned char> listPixels = grayImage.getPixels();
+	ofPixels_<unsigned char> listPixels = gray_image.getPixels();
 
 	for (int y = 0; y < h; y++)
 	{
@@ -286,50 +361,7 @@ void ofApp::showHistogramOf(ofImage grayImage) {
 		points.emplace_back(i + 1, histogram[i]);
 	}
 
-	drawPlotOnScreen(points, "GrayScale Histogram", "x", "y", 40000);
+	draw_plot_on_screen(points, "GrayScale Histogram", "x", "y", 40000);
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg) {
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo) {
-
-}
+/*-----------------------------------------------------------------------------------------------------*/
--- src/ofApp.h
+++ src/ofApp.h
@@ -7,38 +7,46 @@
 class ofApp : public ofBaseApp{
 
 	public:
-		void setup();
-		void update();
-		void draw();
+		void setup() override;
+		void update() override;
+		void draw() override;
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+		void keyPressed(int key) override;
 
 		// my Methods
-		ofImage getGrayScaledImageOf(ofImage image);
+		static ofImage get_gray_scaled_image_of(ofImage image);
 
-		ofImage LUTBlackAndWhite(ofImage imageIn);
+		static ofImage lut_black_and_white(const ofImage& image_in, int threshold);
 
-		ofImage LUTVShape(ofImage imageIn);
+		static ofImage lut_v_shape(const ofImage& image_in);
 
-		void showHistogramOf(ofImage grayImage);
+		static ofImage adaptive_binarisation(const ofImage& image_in);
 
-		int countValueOccurences(int num, int* arr, int length);
+		static ofImage resize_image(ofImage image_in, float scale_down_value);
 
-		void drawPlotOnScreen(vector<ofxGPoint> points, string plotName, string xAxisName, string yAxisName, int plotHeight);
+		void show_histogram_of(ofImage gray_image);
+
+		static std::vector<int> get_histogram_array(const ofImage& gray_image);
+
+		void draw_plot_on_screen(const vector<ofxGPoint>& points, string plot_name, string x_axis_name, string y_axis_name, const int plot_height);
 
 		// Gui
 		ofxPanel gui;
-		ofxIntSlider intSlider;
+		ofxIntSlider int_slider;
 		ofxGPlot plot;
+
+		// keep code
+		/*
+		// find max element in array
+		auto max2 = * max_element(histogram_values.begin(), histogram_values.end());
+
+		// find max element position in array
+		cout << distance(val2.begin(), max_element(val2.begin(), val2.end()));
+
+		// sum values. if you want to sum float values, use 0.0 at end
+		const auto sum_of_elems = std::accumulate(histogram_values.begin()+ index_1, histogram_values.end() - index_2, 0);
+
+		*/
+		
 		
 };
